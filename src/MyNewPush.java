f78741c51130ef9eed8ffeb37d644e9b47dbb5d7
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-08 12:00:32 +0530

    MyNewPush.java

---
+++ src/MyNewPush.java
@@ -0,0 +1,8 @@
+public class MyNewPush {
+    public static void main(String[] args) {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    System.out.printf("Sample program to push the code to git");
+
+}
+}
