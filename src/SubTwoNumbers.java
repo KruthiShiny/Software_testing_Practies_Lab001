38012e41071a017e763342c2c785a75f2a1fb29a
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-09 12:31:08 +0530

    C:\Users\kruthi\Desktop\SoftwareTestingPractices\SoftwareTestingPractices\src\SubTwoNumbers.java

---
+++ src/SubTwoNumbers.java
@@ -0,0 +1,8 @@
+public class SubTwoNumbers {
+    public static void main(String[] agrs){
+       int i=2;
+       int j=5;
+       int diff=j-i;
+        System.out.println("the difference is "+diff);
+    }
+}
