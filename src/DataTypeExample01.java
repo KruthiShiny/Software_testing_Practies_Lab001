550653c2effe95d69c3465f96340a5d1be50e5e9
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-13 07:35:55 +0530

    add AddingTwoNumbers

---
+++ src/DataTypeExample01.java
@@ -0,0 +1,7 @@
+public class DataTypeExample01 {
+    public static void main(String[] args) {
+       // int t; this doesnot work, gives a compile error, always variables need to be inisialised.
+        int t = 1;
+        System.out.println(t);
+    }
+}
---
+++ src/DataTypeExample02.java
@@ -0,0 +1,12 @@
+public class DataTypeExample02 {
+    public static void main(String[] args) {
+        //for(int i=0; 0 ;i++) for loop doesnot tale a value in comparision, takes boolean
+        // and if true is taken it repeats infinite time
+        // if false is given it says statement cannot be reached
+        for(int i=0; true ;i++) //infinite loop
+        {
+            System.out.println("hi");
+
+        }
+    }
+}
