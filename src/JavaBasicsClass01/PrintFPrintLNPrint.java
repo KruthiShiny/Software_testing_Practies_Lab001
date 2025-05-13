2a6e274b2bcd4016fa3b7273f89e0b009dd60983
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-13 14:43:59 +0530

    add AddingTwoNumbers

---
+++ src/JavaBasicsClass01/PrintFPrintLNPrint.java
@@ -0,0 +1,30 @@
+package JavaBasicsClass01;
+
+public class PrintFPrintLNPrint {
+    public static void main(String[] args) {
+        int i=5;
+        int count =1;
+        System.out.printf(i +"*"+ count+" ="+ i*count +"\n");/*this line was print with printf
+        which says that the line is formated here we can also use %d as we use in C. */
+
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+        count++;
+        System.out.println(i +"*"+ count+" ="+ i*count  );
+    }
+}
