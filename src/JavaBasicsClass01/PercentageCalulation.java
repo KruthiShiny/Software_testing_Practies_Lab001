2db9d2197759044a226ffb8c69256b9b240b34c6
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-17 13:15:57 +0530

    calculating the percentage program

---
+++ src/JavaBasicsClass01/PercentageCalulation.java
@@ -0,0 +1,10 @@
+package JavaBasicsClass01;
+
+public class PercentageCalulation {
+    public static void main(String[] args) {
+        int maxScore=500;
+        int scoreReceived= 460;
+        float percentage = (float)scoreReceived/maxScore*100f;
+        System.out.println(percentage);
+    }
+}
