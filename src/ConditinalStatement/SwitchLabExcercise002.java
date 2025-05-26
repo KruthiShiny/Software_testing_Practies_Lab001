edb989fbed28597398a1c02547637ce1f861ea61
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-26 18:56:47 +0530

    Switch statement Asci code program

---
+++ src/ConditinalStatement/SwitchLabExcercise002.java
@@ -0,0 +1,18 @@
+package ConditinalStatement;
+
+public class SwitchLabExcercise002 {
+    public static void main(String[] args) {
+        char ch='B';
+        switch (ch){
+            case 65:
+                System.out.println("the Ascii code number is A");
+                break;
+                case 66:
+                System.out.println("the Ascii code number is B");
+                break;
+
+        }
+
+    }
+
+}
