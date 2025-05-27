917c33d2760efc523135b0ac7af7c3d0f5eae1b1
KruthiShiny <kruthirapally.7@gmail.com>
2025-05-27 13:14:53 +0530

    Factorial program

---
+++ src/TaskPackages/Factorial.java
@@ -0,0 +1,20 @@
+package TaskPackages;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the factorial number:");
+        int n= sc.nextInt();
+        int fact=1;
+        if(n==0){
+            System.out.println(fact);
+        }
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
