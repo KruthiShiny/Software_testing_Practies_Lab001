968262f28e283ea958876d647749b8f4af14b626
KruthiShiny <kruthirapally.7@gmail.com>
2025-06-16 09:33:14 +0530

    Task Related to Abstract class

---
+++ src/AbstractExamples/Parent.java
@@ -0,0 +1,8 @@
+package AbstractExamples;
+
+public abstract class Parent {
+    abstract void show();
+    public void display(){
+        System.out.println("Concrete method in abstract class");
+    }
+}
