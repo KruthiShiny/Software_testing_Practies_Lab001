c0370453c84becc8da96c5aacf75f5ace8527475
KruthiShiny <kruthirapally.7@gmail.com>
2025-07-02 13:56:16 +0530

    Codding MCQ

---
+++ src/SDETbluePrint/StringBasics.java
@@ -0,0 +1,10 @@
+package SDETbluePrint;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        String str="Hello World";
+        System.out.println("Length:"+str.length());
+        System.out.println("First Char"+str.charAt(0));
+        System.out.println("Substring: "+str.substring(6));
+    }
+}
