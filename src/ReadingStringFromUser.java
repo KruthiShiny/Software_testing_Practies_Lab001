24640449a3b18db4e928266dd7907f5758fc6162
KruthiShiny <kruthirapally.7@gmail.com>
2025-07-02 13:43:19 +0530

    Codding MCQ

--- src/ReadingStringFromUser.java
+++ src/ReadingStringFromUser.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ReadingStringFromUser {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("what is your name: ");
-        String name=sc.nextLine();
+       // System.out.print("what is your name: ");
+        //String name=sc.next();
         System.out.print("what is your age: ");
         int age=sc.nextInt();
-        System.out.println("My Name is: "+name+ ",My age is: "+age+".");
+        System.out.println("My Name is: "+ ",My age is: "+age+".");
 
 
 
---
+++ src/SDETbluePrint/VariableDecralation.java
@@ -0,0 +1,11 @@
+package SDETbluePrint;
+
+public class VariableDecralation {
+    public static void main(String[] args) {
+        int i=10;
+        double d=3.14;
+        char ch='A';
+        boolean b=true;
+        System.out.println("Integer:"+i+" Double:"+d+"Boolean:" +b+ "Character:"+ch);
+    }
+}
